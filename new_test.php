2b5821aaafb5be260397c61983b5dd9eb507e802
dastanrab <77535863+dastanrab@users.noreply.github.com>
2021-12-20 23:03:35 +0330

    add back up class

---
+++ new_test.php
@@ -0,0 +1,26 @@
+<?php
+class back_up{
+    public string $first='hi';
+    public string $two='bye';
+    public array $steps;
+  public function __construct()
+  {
+      $this->steps=[1=>'first',2=>'two'];
+  }
+
+    /**
+     * @param string $first
+     */
+    public function setFirst(string $first,int $step=0)
+    {
+        $this->first .= $first;
+        if ($step!=0){
+            $name=$this->steps[$step];
+            $this->$name=$this->$name.$first;
+        }
+        return $this;
+    }
+}
+$test=new back_up();
+$test->setFirst(' !',2);
+print_r($test->two);
